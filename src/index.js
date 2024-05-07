0960801b5c9408a62dd8e647a5b340b565b447d4
verobeach7 <60215757+verobeach7@users.noreply.github.com>
2024-05-07 21:45:30 +0900

    PURE REDUX: Subscribe)

--- src/index.js
+++ src/index.js
@@ -4,30 +4,36 @@ const add = document.getElementById("add");
 const minus = document.getElementById("minus");
 const number = document.querySelector("span");
 
-// Reducer: countStore를 modify하는 함수
-// state를 인자로 받음, Default value를 0으로 설정 / 2번째 인자: modify
+number.innerText = 0;
+
 const countModifier = (count = 0, action) => {
-  console.log(count, action);
-  // Modify state, Action에 따라 state를 수정
   if (action.type === "ADD") {
-    // console.log("They are telling me to add one");
     return count + 1;
   } else if (action.type === "MINUS") {
     return count - 1;
   } else {
-    // Return value는 새로운 data(state)가 됨
     return count;
   }
 };
 
-// countStore: data(state)를 저장하는 곳
-// Reducer의 Return값이 새로운 값이 됨
 const countStore = legacy_createStore(countModifier);
 
-// countModifier에 Action을 보내서 state를 수정
-// dispatch를 사용하면 countModifier를 action({ type: "ADD" })과 함께 호출함
-countStore.dispatch({ type: "ADD" });
-countStore.dispatch({ type: "ADD" });
-countStore.dispatch({ type: "MINUS" });
+const onChange = () => {
+  // console.log(countStore.getState());
+  number.innerText = countStore.getState();
+};
 
-console.log(countStore.getState());
+countStore.subscribe(onChange);
+
+// add.addEventListener("click", () => countStore.dispatch({ type: "ADD" }));
+// minus.addEventListener("click", () => countStore.dispatch({ type: "MINUS" }));
+
+const handleAdd = () => {
+  countStore.dispatch({ type: "ADD" });
+};
+const handleMinus = () => {
+  countStore.dispatch({ type: "MINUS" });
+};
+
+add.addEventListener("click", handleAdd);
+minus.addEventListener("click", handleMinus);
