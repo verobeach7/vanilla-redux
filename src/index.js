2b57ea9093752878d8451343f67280c5724fb12f
verobeach7 <60215757+verobeach7@users.noreply.github.com>
2024-05-07 21:56:49 +0900

    PURE REDUX: Refactoring

--- src/index.js
+++ src/index.js
@@ -6,33 +6,36 @@ const number = document.querySelector("span");
 
 number.innerText = 0;
 
+// string으로 사용하면 잘못 타이핑 했을 때 어디서 오류가 발생했는지 알 수 없음
+// 자동완성기능을 이용할 수 있으며 오타가 발생하면 어디서 에러가 발생했는지 알려줌
+const ADD = "ADD";
+const MINUS = "MINUS";
+
 const countModifier = (count = 0, action) => {
-  if (action.type === "ADD") {
-    return count + 1;
-  } else if (action.type === "MINUS") {
-    return count - 1;
-  } else {
-    return count;
+  // if문 대신 switch 사용
+  switch (action.type) {
+    case ADD:
+      return count + 1;
+    case MINUS:
+      return count - 1;
+    default:
+      return count;
   }
 };
 
 const countStore = legacy_createStore(countModifier);
 
 const onChange = () => {
-  // console.log(countStore.getState());
   number.innerText = countStore.getState();
 };
 
 countStore.subscribe(onChange);
 
-// add.addEventListener("click", () => countStore.dispatch({ type: "ADD" }));
-// minus.addEventListener("click", () => countStore.dispatch({ type: "MINUS" }));
-
 const handleAdd = () => {
-  countStore.dispatch({ type: "ADD" });
+  countStore.dispatch({ type: ADD });
 };
 const handleMinus = () => {
-  countStore.dispatch({ type: "MINUS" });
+  countStore.dispatch({ type: MINUS });
 };
 
 add.addEventListener("click", handleAdd);
