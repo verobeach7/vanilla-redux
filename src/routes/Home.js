d55d951f0d4bc4542c795df075432fbd0991d0bd
verobeach7 <60215757+verobeach7@users.noreply.github.com>
2024-05-12 20:57:28 +0900

    REACT REDUX: mapStateToProps

--- src/routes/Home.js
+++ src/routes/Home.js
@@ -1,6 +1,7 @@
 import React, { useState } from "react";
+import { connect } from "react-redux";
 
-function Home() {
+function Home({ toDos }) {
   const [text, setText] = useState("");
   const onChange = (e) => {
     setText(e.target.value);
@@ -17,9 +18,23 @@ function Home() {
         <input type="text" value={text} onChange={onChange} />
         <button>Add</button>
       </form>
-      <ul></ul>
+      <ul>{JSON.stringify(toDos)}</ul>
     </>
   );
 }
 
-export default Home;
+/* // React Redux: connect - mapStateToProps
+// mapStateToProps를 사용한다는 것은 Redux Store로부터 무엇인가를 가져오고 싶을 것
+function mapStateToProps(state, ownProps) {
+  // state: redux store에 저장되어있는 state
+  // ownProps: Home 컴포넌트에 보내진 props
+  // console.log(state, ownProps);
+  return { sexy: true };
+} */
+
+function mapStateToProps(state) {
+  return { toDos: state };
+}
+
+// getCurrentState에 반환된 props가 Home컴포넌트의 props에 추가됨
+export default connect(mapStateToProps)(Home);
