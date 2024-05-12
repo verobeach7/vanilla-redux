c0834fdde8999c587bd07c2987cb990a5f265843
verobeach7 <60215757+verobeach7@users.noreply.github.com>
2024-05-12 13:03:54 +0900

    PURE REDUX: To Do List(Javascript VS Redux)

--- src/index.js
+++ src/index.js
@@ -4,17 +4,41 @@ const form = document.querySelector("form");
 const input = document.querySelector("input");
 const ul = document.querySelector("ul");
 
-const createToDo = (toDo) => {
+// 방법 1. data 생성(javascript 방식)
+// const toDos = [];
+// 이후 데이터가 수정되면 toDos를 수정하고 다시 상황별로 랜더링을 다시 하는 함수를 작성하는 방식으로 처리해야 함
+
+// 방법 2. data 생성(redux 이용 방식)
+const ADD_TODO = "ADD_TODO";
+const DELETE_TODO = "DELETE TODO";
+
+const reducer = (state = [], action) => {
+  console.log(action); // {type: 'ADD_TODO', text: 'asdf'}
+  switch (action.type) {
+    case ADD_TODO:
+      return [];
+    case DELETE_TODO:
+      return [];
+    default:
+      return state;
+  }
+};
+
+const store = legacy_createStore(reducer);
+
+/* const createToDo = (toDo) => {
   const li = document.createElement("li");
   li.innerText = toDo;
   ul.appendChild(li);
-};
+}; */
 
 const onSubmit = (e) => {
   e.preventDefault();
   const toDo = input.value;
   input.value = "";
-  createToDo(toDo);
+  // createToDo(toDo);
+  // dispatch를 통해서 reducer와 커뮤니케이션 할 수 있음. 원하는 모든 정보 보낼 수 있음.
+  store.dispatch({ type: ADD_TODO, text: toDo });
 };
 
 form.addEventListener("submit", onSubmit);
