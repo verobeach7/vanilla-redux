3148b8434f28aba0664feda606d1cff9a248d52a
verobeach7 <60215757+verobeach7@users.noreply.github.com>
2024-05-12 16:25:29 +0900

    PURE REDUX: To Do List(Delete To Do)

--- src/index.js
+++ src/index.js
@@ -7,13 +7,27 @@ const ul = document.querySelector("ul");
 const ADD_TODO = "ADD_TODO";
 const DELETE_TODO = "DELETE TODO";
 
+// Action Creator: 일반적으로 Action을 Reducer 위쪽에 따로 만들어서 사용함
+const addToDo = (text) => {
+  return { type: ADD_TODO, text, id: Date.now() };
+};
+
+const deleteToDo = (id) => {
+  return {
+    type: DELETE_TODO,
+    id,
+  };
+};
+
 const reducer = (state = [], action) => {
   switch (action.type) {
     case ADD_TODO:
       // return [...state, { text: action.text, id: action.id }];
-      return [{ text: action.text, id: action.id }, ...state];
+      const newToDoObj = { text: action.text, id: action.id };
+      return [newToDoObj, ...state];
     case DELETE_TODO:
-      return [];
+      const cleaned = state.filter((toDo) => toDo.id !== action.id);
+      return cleaned;
     default:
       return state;
   }
@@ -23,28 +37,39 @@ const store = legacy_createStore(reducer);
 
 store.subscribe(() => console.log(store.getState()));
 
+const dispatchAddToDo = (text) => {
+  store.dispatch(addToDo(text));
+};
+
+const dispatchDeleteToDo = (e) => {
+  // console.log(e.target.parentNode.id); // 부모 node를 가져올 수 있음
+  // html에서 넘어오는 id는 숫자이지만 string 형태로 넘어옴
+  const id = parseInt(e.target.parentNode.id);
+  store.dispatch(deleteToDo(id));
+};
+
 const paintToDos = () => {
   const toDos = store.getState();
   ul.innerHTML = "";
   toDos.forEach((toDo) => {
     const li = document.createElement("li");
+    const btn = document.createElement("button");
+    btn.innerText = "DEL";
+    btn.addEventListener("click", dispatchDeleteToDo);
     li.id = toDo.id;
     li.innerText = toDo.text;
+    li.appendChild(btn);
     ul.appendChild(li);
   });
 };
 
 store.subscribe(paintToDos);
 
-const addToDo = (text) => {
-  store.dispatch({ type: ADD_TODO, text, id: Date.now() });
-};
-
 const onSubmit = (e) => {
   e.preventDefault();
   const toDo = input.value;
   input.value = "";
-  addToDo(toDo);
+  dispatchAddToDo(toDo);
 };
 
 form.addEventListener("submit", onSubmit);
